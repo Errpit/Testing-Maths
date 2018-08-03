de48bc2c98923e7d49d1df72309637c35bec3d55
arpit <arpitsharma@innotical.com>
2018-08-03 18:28:36 +0530

    Added Fire and Light rooms

---
+++ Assets/Lighting Lab/Interact.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interact : MonoBehaviour {
+
+    public Button interactButton;
+    bool interactButtonDown=false;
+    Rigidbody rig;
+    public float force=1f;
+	// Use this for initialization
+	void Start () {
+       interactButton.gameObject.SetActive(false);
+	}
+
+    public void SetButton()
+    {
+        Debug.Log("NearDoor");
+        interactButtonDown = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Door"))
+        {
+            interactButtonDown = false;
+                interactButton.gameObject.SetActive(true);
+            rig = other.gameObject.GetComponent<Rigidbody>();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Door") && rig!=null)
+        {
+            if (interactButtonDown)
+            {
+                Debug.Log("Attempting open");
+                rig.AddForce(transform.forward * force);
+                interactButtonDown = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Door"))
+        {
+            interactButton.gameObject.SetActive(false);
+            rig = null;
+            interactButtonDown = false;
+        }
+    }
+   
+}
---
+++ Assets/lever/SuckIt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuckIt : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+            Debug.Log("sucked");
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.SetActive(false);
+
+        }
+    }
+}
